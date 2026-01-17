498f370f96fa4e09ab2381e49172b58fae06a3b8
Webber <webber_lee@outlook.com>
2026-01-18 02:55:55 +0800

    feat(QuanX): 对StdVLESS解析方式，增加TLSSecure字段的赋值

--- src/parser/subparser.cpp
+++ src/parser/subparser.cpp
@@ -2126,7 +2126,7 @@ void explodeAnyTLS(std::string anytls, Proxy &node) {
 }
 
 void explodeStdVLESS(std::string vless, Proxy &node) {
-    std::string add, port, uuid, sni, alpn, net, type, mode, host, path, fingerprint, remarks, addition, flow, xtls, public_key, short_id;
+    std::string add, port, uuid, sni, alpn, net, type, mode, host, path, fingerprint, remarks, addition, flow, xtls, public_key, short_id, security, tls;
     tribool tfo, scv;
     std::string decoded, userinfo, hostinfo;
     string_array user_parts;
@@ -2187,6 +2187,7 @@ void explodeStdVLESS(std::string vless, Proxy &node) {
         flow = getUrlArg(addition, "flow");
         xtls = getUrlArg(addition, "xtls");
         public_key = getUrlArg(addition, "pbk");
+        security = getUrlArg(addition, "security");
         short_id = getUrlArg(addition, "sid");
         tfo = tribool(getUrlArg(addition, "tfo"));
         std::string insecure_val = getUrlArg(addition, "insecure");
@@ -2226,7 +2227,7 @@ void explodeStdVLESS(std::string vless, Proxy &node) {
 
     if (remarks.empty())
         remarks = add + ":" + port;
-
+    node.TLSSecure = security == "tls" || security == "reality";
     vlessConstruct(node, VLESS_DEFAULT_GROUP, remarks, add, port, uuid, sni, alpn, type, net, mode, host, path, fingerprint, flow, xtls, public_key, short_id, "", tribool(), tfo, scv, "");
 }
 
